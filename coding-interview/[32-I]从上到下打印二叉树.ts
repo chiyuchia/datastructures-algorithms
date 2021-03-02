5b62c2b1ff8af60751444900986a8d883993a9b1
Yoga <chiyuchiascau@gmail.com>
2021-03-03 00:44:46 +0800

    feat: add coding-interview-32-I

---
+++ coding-interview/[32-I]从上到下打印二叉树.ts
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function levelOrder(root: TreeNode | null): number[] {
+  const ans: number[] = [];
+  if (!root) return ans;
+  const queue = [root];
+  while (queue.length) {
+    const node = queue.shift();
+    if (node.left) queue.push(node.left);
+    if (node.right) queue.push(node.right);
+    ans.push(node.val);
+  }
+  return ans;
+}
