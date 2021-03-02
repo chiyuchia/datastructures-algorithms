450a2af2672ab7deb5e0f755b198d8ef64b069e8
Yoga <chiyuchiascau@gmail.com>
2021-03-02 17:16:04 +0800

    feat: add leetcode-231

---
+++ leetcode/bit-manipulation/[231]2-的幂.ts
@@ -0,0 +1,41 @@
+/*
+ * @lc app=leetcode.cn id=231 lang=typescript
+ *
+ * [231] 2的幂
+ *
+ * https://leetcode-cn.com/problems/power-of-two/description/
+ *
+ * algorithms
+ * Easy (48.96%)
+ * Likes:    286
+ * Dislikes: 0
+ * Total Accepted:    99.3K
+ * Total Submissions: 201.7K
+ * Testcase Example:  '1'
+ *
+ * 给定一个整数，编写一个函数来判断它是否是 2 的幂次方。
+ *
+ * 示例 1:
+ *
+ * 输入: 1
+ * 输出: true
+ * 解释: 2^0 = 1
+ *
+ * 示例 2:
+ *
+ * 输入: 16
+ * 输出: true
+ * 解释: 2^4 = 16
+ *
+ * 示例 3:
+ *
+ * 输入: 218
+ * 输出: false
+ *
+ */
+
+// @lc code=start
+function isPowerOfTwo(n: number): boolean {
+  return n >= 1 && (n & (n - 1)) === 0;
+}
+// @lc code=end
