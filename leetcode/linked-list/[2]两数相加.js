d83bc3fac9966444e7b2a67efd1896d91d34ab41
Yoga <chiyuchiascau@gmail.com>
2021-03-03 22:38:22 +0800

    feat: add leetcode-2

--- leetcode/linked-list/[2]两数相加.js
+++ leetcode/linked-list/[2]两数相加.js
@@ -36,18 +36,32 @@
  *     this.next = null;
  * }
  */
+
+function ListNode(val) {
+  this.val = val;
+  this.next = null;
+}
+
 /**
  * @param {ListNode} l1
  * @param {ListNode} l2
  * @return {ListNode}
  */
 var addTwoNumbers = function (l1, l2) {
-  const reverseLinkList = (head) => {
-    let currentNode = link.head;
-    while (currentNode !== null) {
-      // const nextNode = currentNode.next;
-      currentNode = currentNode.next;
-    }
-  };
+  let dummy = new ListNode(-1);
+  let curr = dummy;
+  let carry = 0;
+  while (l1 || l2) {
+    const val1 = l1 ? l1.val : 0;
+    const val2 = l2 ? l2.val : 0;
+    const sum = val1 + val2 + carry;
+    curr.next = new ListNode(sum % 10);
+    curr = curr.next;
+    carry = Math.floor(sum / 10);
+    if (l1) l1 = l1.next;
+    if (l2) l2 = l2.next;
+  }
+  if (carry) curr.next = new ListNode(carry);
+  return dummy.next;
 };
 // @lc code=end
