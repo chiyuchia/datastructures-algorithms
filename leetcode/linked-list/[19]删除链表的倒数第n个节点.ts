430e6542457def2a27eedd14fe19add5d0a62457
Yoga <chiyuchiascau@gmail.com>
2021-03-03 15:50:49 +0800

    feat: add leetcode-19

---
+++ leetcode/linked-list/[19]删除链表的倒数第n个节点.ts
@@ -0,0 +1,61 @@
+/*
+ * @lc app=leetcode.cn id=19 lang=typescript
+ *
+ * [19] 删除链表的倒数第N个节点
+ *
+ * https://leetcode-cn.com/problems/remove-nth-node-from-end-of-list/description/
+ *
+ * algorithms
+ * Medium (40.71%)
+ * Likes:    1178
+ * Dislikes: 0
+ * Total Accepted:    308.9K
+ * Total Submissions: 758.1K
+ * Testcase Example:  '[1,2,3,4,5]\n2'
+ *
+ * 给定一个链表，删除链表的倒数第 n 个节点，并且返回链表的头结点。
+ *
+ * 示例：
+ *
+ * 给定一个链表: 1->2->3->4->5, 和 n = 2.
+ *
+ * 当删除了倒数第二个节点后，链表变为 1->2->3->5.
+ *
+ *
+ * 说明：
+ *
+ * 给定的 n 保证是有效的。
+ *
+ * 进阶：
+ *
+ * 你能尝试使用一趟扫描实现吗？
+ *
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function removeNthFromEnd(head: ListNode | null, n: number): ListNode | null {
+  const dummy = new ListNode(-1, head);
+  let fast: ListNode | null = head;
+  let slow: ListNode | null = dummy;
+  for (let i = 0; i < n; i++) fast = fast.next;
+  while (fast) {
+    fast = fast.next;
+    slow = slow.next;
+  }
+  slow.next = slow.next.next;
+  return dummy.next;
+}
+
+// @lc code=end
