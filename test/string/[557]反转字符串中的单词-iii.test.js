e445d2cce72266be537ff71485d6e04b9c36d20a
Yoga <chiyuchiascau@gmail.com>
2021-03-03 12:11:27 +0800

    chore: delete testing

--- test/string/[557]反转字符串中的单词-iii.test.js
+++
@@ -1,7 +0,0 @@
-import revertByWorld from '../../code/string/[557]反转字符串中的单词-iii'
-
-test("reverseWords: Let's take LeetCode contest", () => {
-  expect(revertByWorld("Let's take LeetCode contest")).toBe(
-    "s'teL ekat edoCteeL tsetnoc",
-  )
-})
