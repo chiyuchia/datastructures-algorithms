0a377386282a57dc1c72c0db7abb308a18e6749b
Yoga <chiyuchiascau@gmail.com>
2021-03-04 10:34:23 +0800

    fix: correct BFS

--- data-structrues/BFS.ts
+++ data-structrues/BFS.ts
@@ -2,15 +2,17 @@
  * @Author: Yoga
  * @Date: 2021-02-24 00:36:48
  * @LastEditors: 纪煜佳
- * @LastEditTime: 2021-03-03 22:42:37
+ * @LastEditTime: 2021-03-04 10:29:58
  * @Description: 广度优先搜索
  */
 
-const breadthFirstSearch = (head: ListNode | null, callback?: (node: ListNode) => void) => {
-  if (!head) return;
-  let curr = head;
-  while (curr) {
-    if (callback) callback(curr);
-    curr = curr.next;
+const breadthFirstSearch = (node: TreeNode | null, callback?: (node: TreeNode) => void) => {
+  if (!node) return;
+  const queue: TreeNode[] = [node];
+  while (queue) {
+    const node = queue.shift();
+    if (callback) callback(node);
+    if (node.left) queue.push(node.left);
+    if (node.right) queue.push(node.right);
   }
 };
