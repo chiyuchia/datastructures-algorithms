3248a083c2e96c8bbc3398ae4538aa5f55def656
Yoga <chiyuchiascau@gmail.com>
2021-06-17 01:33:19 +0800

    feat: 添加 [112] 路径总和 题解

---
+++ leetcode/depth-first-search/[112]路径总和.ts
@@ -0,0 +1,76 @@
+/*
+ * @lc app=leetcode.cn id=112 lang=typescript
+ *
+ * [112] 路径总和
+ *
+ * https://leetcode-cn.com/problems/path-sum/description/
+ *
+ * algorithms
+ * Easy (52.29%)
+ * Likes:    598
+ * Dislikes: 0
+ * Total Accepted:    218.6K
+ * Total Submissions: 417.8K
+ * Testcase Example:  '[5,4,8,11,null,13,4,7,2,null,null,null,1]\n22'
+ *
+ * 给你二叉树的根节点 root 和一个表示目标和的整数 targetSum ，判断该树中是否存在 根节点到叶子节点
+ * 的路径，这条路径上所有节点值相加等于目标和 targetSum 。
+ *
+ * 叶子节点 是指没有子节点的节点。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：root = [5,4,8,11,null,13,4,7,2,null,null,null,1], targetSum = 22
+ * 输出：true
+ *
+ *
+ * 示例 2：
+ *
+ *
+ * 输入：root = [1,2,3], targetSum = 5
+ * 输出：false
+ *
+ *
+ * 示例 3：
+ *
+ *
+ * 输入：root = [1,2], targetSum = 0
+ * 输出：false
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 树中节点的数目在范围 [0, 5000] 内
+ * -1000
+ * -1000
+ *
+ *
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function hasPathSum(root: TreeNode | null, targetSum: number): boolean {
+  if (!root) return false;
+  if (!root.left && !root.right) return targetSum === root.val;
+  return hasPathSum(root.left, targetSum - root.val) || hasPathSum(root.right, targetSum - root.val);
+}
+// @lc code=end
