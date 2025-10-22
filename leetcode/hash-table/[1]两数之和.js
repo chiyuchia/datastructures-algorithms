bdac66bd1ea6be068cad7ad9b647b1966810bc4e
Yujia Ji <chiyuchia@outlook.com>
2025-10-22 15:44:30 +0800

    feat: 新增「1.两数之和」Hash Table 解法的 TS 实现

--- leetcode/hash-table/[1]两数之和.js
+++ leetcode/hash-table/[1]两数之和.js
@@ -10,18 +10,18 @@
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function(nums, target) {
-  let hashMap = {}
+var twoSum = function (nums, target) {
+  let hashMap = {};
   for (let i = 0; i < nums.length; i++) {
-    const complement = target - nums[i]
+    const complement = target - nums[i];
     if (hashMap.hasOwnProperty(complement)) {
-      return [hashMap[complement], i]
+      return [hashMap[complement], i];
     } else {
-      const key = nums[i]
-      hashMap[key] = i
+      const key = nums[i];
+      hashMap[key] = i;
     }
   }
-}
+};
 // @lc code=end
 
-export default twoSum
+export default twoSum;
