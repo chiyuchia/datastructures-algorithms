d385325b13d58203a78da2e8aec1893950164c7a
chiyuchia <chiyuchiascau@gmail.com>
2025-10-22 16:16:04 +0800

    feat: 添加[283]移动零 题解

---
+++ leetcode/two-pointers/[283]移动零.ts
@@ -0,0 +1,22 @@
+/*
+ * @lc app=leetcode.cn id=283 lang=typescript
+ *
+ * [283] 移动零
+ */
+
+// @lc code=start
+/**
+ Do not return anything, modify nums in-place instead.
+ */
+function moveZeroes(nums: number[]): void {
+  let left = 0;
+  let right = 0;
+  while (right < nums.length) {
+    if (nums[right] !== 0) {
+      [nums[left], nums[right]] = [nums[right], nums[left]];
+      left++;
+    }
+    right++;
+  }
+}
+// @lc code=end
