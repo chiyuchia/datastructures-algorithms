6404cb1befff7727bfc3f6ac6d487950a5f59c3d
chiyuchia <chiyuchiascau@gmail.com>
2025-10-23 16:49:32 +0800

    feat: 添加[226]翻转二叉树 题解

---
+++ leetcode/tree/[226]翻转二叉树.ts
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode.cn id=226 lang=typescript
+ *
+ * [226] 翻转二叉树
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function invertTree(root: TreeNode | null): TreeNode | null {
+  if (!root) {
+    return root;
+  }
+  [root.left, root.right] = [invertTree(root.right), invertTree(root.left)];
+  return root;
+}
+// @lc code=end
