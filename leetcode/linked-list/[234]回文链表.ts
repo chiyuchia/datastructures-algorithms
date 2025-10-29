cdf61a8a8635c1482999fee69911417b9ad9a62c
chiyuchia <chiyuchiascau@gmail.com>
2025-10-29 18:05:46 +0800

    feat: 添加[234]回文链表 实现

---
+++ leetcode/linked-list/[234]回文链表.ts
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode.cn id=234 lang=typescript
+ *
+ * [234] 回文链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function isPalindrome(head: ListNode | null): boolean {
+  const arr: number[] = [];
+  while (head) {
+    arr.push(head.val);
+    head = head.next;
+  }
+  return arr.toString() === arr.reverse().toString();
+}
+// @lc code=end
